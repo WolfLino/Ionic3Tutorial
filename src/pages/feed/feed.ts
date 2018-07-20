422e377bab4775810bb0527550f2bbc5b99cdb92
WolfLino <ulissese.lino@outlook.com>
2018-07-20 18:49:18 -0300

    Atualização do feed.

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -15,6 +15,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
   public nome_usuario: string = "Usuário";
+  public objeto_feed = {
+    titulo:"Usuário",
+    data:"14 de novembro, 1998",
+    descricao:"Primeiro post do meu feed.",
+    qnt_likes:12,
+    qnt_comentarios:4,
+    hora_post:"11hr atrás"
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
